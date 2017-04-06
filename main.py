34248dc392560248195b4da80d384fa160e873fb
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-04-05 17:56:20 -0700

    Add styles dir.  Add beginning of secure hashing/salting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import re
+import random
+import hashlib
+import hmac
+from string import letters
 import jinja2
 import webapp2
 
@@ -13,15 +17,25 @@ JINJA_ENV = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+SECRET = '$w0rdf1sh'
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+def make_secure_val(val):
+    return "%s|%s" % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
 def valid_username(username):
     return username and USER_RE.match(username)
 
-PASS_RE = re.compile(r"^.{3,20}$")
 def valid_password(password):
     return password and PASS_RE.match(password)
 
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
